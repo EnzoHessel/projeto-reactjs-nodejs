3341bdb3046e49d067bb7e1079a94d5461897d48
enzohessel <enzo.carmo@salg.com.br>
2023-07-31 15:18:11 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import Login from './components/Login';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Login />
+      </header>
+    </div>
+  );
+}
+
+export default App;
